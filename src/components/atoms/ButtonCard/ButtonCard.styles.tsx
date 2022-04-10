89b7821d3bf4df28dce37060788196a02f1ef15f
Edison Chancusig <47606288+ediferchancusig@users.noreply.github.com>
2022-04-10 11:24:12 -0500

    Add ButtonCard atom

---
+++ src/components/atoms/ButtonCard/ButtonCard.styles.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Styles = styled.div`
+  .button button{
+    width: 40%;
+    height: 40px;
+    color: var(--dark-red);
+    background-color: var(--light-white);
+    border-radius: 5px;
+    border-width: 0;
+    margin: 3px;
+    padding: 10px;
+  }
+`
+export default Styles
---
+++ src/components/atoms/ButtonCard/ButtonCard.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react'
+import ButtonCard from './ButtonCard'
+
+describe('ButtonCard atom', () => {
+
+  it('Should be defined', async () => {
+    expect(ButtonCard).toBeDefined()
+  })
+
+  it('Should render', () => {
+    const onClick = jest.fn()
+    expect(() => {
+      render(
+        <ButtonCard
+          onClick={onClick}>Hazme click
+        </ButtonCard>)
+    }).not.toThrow()
+  })
+
+})
